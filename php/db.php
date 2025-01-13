cb3c96545ba7c0078709890b18a88d5317e518e2
harm321 <danilka_bondar@list.ru>
2025-01-13 22:28:24 +0500

    php файлы

---
+++ php/db.php
@@ -0,0 +1,13 @@
+<?php
+$serverName = "localhost";  
+$database = "gtklib";
+$username = "test";  
+$password = "test"; 
+
+try {
+    $conn = new PDO("sqlsrv:Server=$serverName;Database=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Ошибка подключения: " . $e->getMessage());
+}
+?>
